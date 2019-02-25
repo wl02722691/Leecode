99ea0de319638019483071ac51db10035c8b06e3
張書涵 <zhangshuhan@zhangshuhans-MacBook-Pro.local>
2019-02-25 20:59:28 +0800

    Fix 7

---
+++ 122. Best Time to Buy and Sell Stock II/122. Best Time to Buy and Sell Stock II/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  122. Best Time to Buy and Sell Stock II
+//
+//  Created by 張書涵 on 2019/2/25.
+//  Copyright © 2019 張書涵. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
--- 7. Reverse Integer/7. Reverse Integer/ViewController.swift
+++ 7. Reverse Integer/7. Reverse Integer/ViewController.swift
@@ -9,23 +9,26 @@
 import UIKit
 class Solution {
     func reverse(_ x: Int) -> Int {
-      
-        var reverArr = [String]()
-        reverArr = String(x).map{"\($0)"}.reversed()
-        print(reverArr)
         
-        if x <= 0 {
-         if let r = Int32
-            
-            
+        var reversedXArr = [String]()
+        reversedXArr = String(x).map{"\($0)"}.reversed()
+        
+        if x >= 0 {
+            if let r = Int32(reversedXArr.joined()) {
+                return Int(r)
+            } else {
+                return 0
+            }
         } else {
-            
+            reversedXArr.remove(at: reversedXArr.count - 1)
+            reversedXArr.insert("-", at: 0)
+            if  let r = Int32(reversedXArr.joined()) {
+                return Int(r)
+            } else {
+                return 0
+            }
         }
-     
-        return 1
     }
-    
-    
 }
 
 class ViewController: UIViewController {
